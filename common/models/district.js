20e929a7eb1ddb5d3d3824101a6777f0d350bd65
haxxy09 <htwalibu@gmail.com>
2018-05-28 18:54:35 +0200

    added getName method on model District

--- common/models/district.js
+++ common/models/district.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(District) {
-  District.validatesUniquenessOf("name");
+  // District.validatesUniquenessOf("name");
+  District.getName = function(keyword, cb) {
+    District.find(keyword, function(err, instance) {
+      var response = "result" + instance.name;
+      cb(null, response);
+      console.log(response);
+    });
+  };
+  District.remoteMethod(
+    'getName', {
+      http: {
+        path: '/getname',
+        verb: 'get'
+      },
+      accepts: {arg: 'keyword', type: 'string', http: {source: 'query'}},
+      returns: {
+        arg: 'name',
+        type: 'string'
+      }
+    }
+  );
 };
